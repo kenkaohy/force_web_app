6b183bb42ddc45be4775df857fd377298d8f4a62
kenkaohy <ken.kao.hy@gmail.com>
2016-10-21 14:49:08 -0700

    Add project files.

---
+++ force_web_app/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(force_web_app.Startup))]
+namespace force_web_app
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
